04e436fc58a1b6f342bbe8fe837b3e6d28d023fd
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2024-10-08 13:17:14 +0400

    finished part 3

--- main.py
+++ main.py
@@ -6,16 +6,26 @@ Token = namedtuple("Token", ["type", "value"])
 RootNode = namedtuple("RootNode", "body")
 KeyValueNode = namedtuple("KeyValueNode", ["key", "value"])
 StringNode = namedtuple("StringNode", "value")
+NumericNode = namedtuple("NumericNode", "value")
+BooleanNode = namedtuple("BooleanNode", "value")
+NullNode = namedtuple("NullNode", "value")
 
 
 def tokenise(inp: str) -> list[Token]:
     OPEN_BRACE = r"(?P<OPEN_BRACE>{)"
     CLOSE_BRACE = r"(?P<CLOSE_BRACE>})"
     STRING = r'(?P<STRING>"\w+")'
+    NUMERIC = r"(?P<NUMERIC>[-]?\d+\.?\d+)"
     COLON = r"(?P<COLON>:)"
+    BOOLEAN = r"(?P<BOOLEAN>true|false)"
     COMMA = r"(?P<COMMA>,)"
+    NULL = r"(?P<NULL>null)"
     WS = r"(?P<WS>\s+)"
-    pattern = re.compile("|".join([OPEN_BRACE, CLOSE_BRACE, STRING, WS, COLON, COMMA]))
+    pattern = re.compile(
+        "|".join(
+            [OPEN_BRACE, NULL, BOOLEAN, CLOSE_BRACE, NUMERIC, STRING, WS, COLON, COMMA]
+        )
+    )
 
     def generate_tokens(pat, text):
         scanner = pat.scanner(text)
@@ -24,6 +34,7 @@ def tokenise(inp: str) -> list[Token]:
                 case "WS":
                     continue
                 case "STRING":
+                    # remove the surrounding quotes
                     yield Token(m.lastgroup, m.group()[1:-1])
                 case _:
                     yield Token(m.lastgroup, m.group())
@@ -35,17 +46,48 @@ def parse(tokens: list[Token]) -> RootNode:
     def consume(expected_type: str):
         next_token = tokens.pop(0)
         if next_token.type != expected_type:
-            raise Exception(f"Parse error at {next_token}")
+            raise RuntimeError(f"Parse error at {next_token}")
         return next_token.value
 
-    def parse_body():
+    def peek(expected_type: str):
+        return tokens[0].type == expected_type
+
+    def parse_numeric() -> NumericNode:
+        return NumericNode(value=float(consume("NUMERIC")))
+
+    def parse_boolean() -> BooleanNode:
+        return BooleanNode(value=bool(consume("BOOLEAN")))
+
+    def parse_value() -> StringNode | NumericNode | BooleanNode:
+        if peek("STRING"):
+            return StringNode(consume("STRING"))
+        elif peek("NUMERIC"):
+            return parse_numeric()
+        elif peek("BOOLEAN"):
+            return parse_boolean()
+        elif peek("NULL"):
+            return NullNode(consume("NULL"))
+        else:
+            raise RuntimeError(
+                "Could not parse value, ensure it is of type [number, string, boolean, null]"
+            )
+
+    def parse_key_value_pair() -> KeyValueNode:
         key = StringNode(value=consume("STRING"))
         consume("COLON")
-        value = StringNode(value=consume("STRING"))
-        return RootNode(body=[KeyValueNode(key, value)])
+        value = parse_value()
+        return KeyValueNode(key, value)
+
+    def parse_body() -> list[KeyValueNode]:
+        body = []
+        body.append(parse_key_value_pair())
+        while peek("COMMA"):
+            consume("COMMA")
+            body.append(parse_key_value_pair())
+        return body
 
     consume("OPEN_BRACE")
-    ast = parse_body()
+    ast = RootNode(body=parse_body())
     consume("CLOSE_BRACE")
     return ast
 
